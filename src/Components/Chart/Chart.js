5957f4ea36f856cab4b9f3a188329fd968095744
Devildk384 <durgeshkumar384@gmail.com>
2020-12-25 22:54:31 +0530

    first commit

---
+++ src/Components/Chart/Chart.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Line } from 'react-chartjs-2'
+import "./Chart.css"
+
+const data = {
+    labels: [ '', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'],
+    datasets: [
+      {
+        data: [ 825, 2514,789, 1054 , 546, 1758],
+        fill: false,
+        backgroundColor: 'black',
+        borderColor: 'black',
+      },
+    ],
+  }
+
+function Chart() {
+   
+
+    return (
+       
+
+        <Line width={400} height={150} data={data}   />
+            
+    )
+}
+
+export default Chart
